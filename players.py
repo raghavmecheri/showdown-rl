03ac3a16dddb13b0d4a519ef52dd9b613c37e563
Raghav Mecheri (Linux) <raghav@mecheri.in>
2021-04-19 19:56:31 +0000

    Added Minimax player

--- players.py
+++ players.py
@@ -16,12 +16,16 @@ def _vectorise_stat(x):
     }
     return vects[x]
 
+def _score_move(move):
+    return move.accuracy * (move.base_power * move.expected_hits) * (1.2 if move.status else 1)
+
 class SimpleRLPlayer(Gen8EnvSinglePlayer):
     def embed_battle(self, battle):
         moves_base_power = -np.ones(4)
         moves_base_status = np.zeros((4, 6))
         moves_base_heal = np.zeros(4)
         moves_dmg_multiplier = np.ones(4)
+        moves_base_accuracy = np.zeros(4)
 
         for i, move in enumerate(battle.available_moves):
             moves_base_heal[i] = move.heal - move.recoil
@@ -32,6 +36,7 @@ class SimpleRLPlayer(Gen8EnvSinglePlayer):
             moves_base_power[i] = (
                 move.base_power / 100
                 )  # Simple rescaling to facilitate learning
+            moves_base_accuracy[i] = move.accuracy
             if move.type:
                 moves_dmg_multiplier[i] = move.type.damage_multiplier(
                     battle.opponent_active_pokemon.type_1,
@@ -51,6 +56,7 @@ class SimpleRLPlayer(Gen8EnvSinglePlayer):
             [
                 moves_base_power,
                 moves_dmg_multiplier,
+                moves_base_accuracy,
                 moves_base_heal,
                 moves_base_status.flatten(order='C'),
                 [remaining_mon_team, remaining_mon_opponent],
@@ -68,7 +74,7 @@ class MaxDamagePlayer(RandomPlayer):
         # If the player can attack, it will
         if battle.available_moves:
             # Finds the best move among available ones
-            best_move = max(battle.available_moves, key=lambda move: move.base_power)
+            best_move = max(battle.available_moves, key=lambda move: _score_move(move.base_power))
             return self.create_order(best_move)
 
         # If no attack is available, a random switch will be made
@@ -84,35 +90,57 @@ class RandomizedMaxDamagePlayer(RandomPlayer):
             if np.random.uniform() < epsilon:
                 return self.choose_random_move(battle)
 
-            best_move = max(battle.available_moves, key=lambda move: move.base_power)
+            best_move = max(battle.available_moves, key=lambda move: _score_move(move.base_power))
             return self.create_order(best_move)
         else:
             return self.choose_random_move(battle)
+
+class Minimax:
+    def __init__(self, moves, opp_moves):
+        self.moves = moves
+        self.opp_moves = opp_moves
+        self.moves_scored = [_score_move(x) for x in moves]
+        self.opp_moves_scored = [-1 * _score_move(x) for x in opp_moves]
+
+    def play_best_move(depth):
+
+        def minimax(val, scores, opp_scores, max_depth, depth, is_maxer):
+            if depth == max_depth:
+                return val
+
+            if is_maxer:
+                best_val = -1
+                for move in scores:
+                    val = minimax(val, scores, opp_scores, max_depth, depth + 1, False)
+                    best_val = max(best_val, value)
+                best_val
+
+            else:
+                best_val = float('inf')
+                for move in opp_scores:
+                    val = minimax(val, scores, opp_scores, max_depth, depth + 1, True)
+                    best_val = min(val, best_val)
+                return best_val
+
+        best_val, best_move = -1, -1
+
+        for i in range(self.moves_scored):
+            val = minimax(0, self.moves_scored, self.opp_moves_scored, depth, 0, True)
+            if val > best_val:
+                best_val, best_move = val, i
+
+        return self.moves[best_move]
 
 class MinimaxPlayer(RandomPlayer):
     def play_minimax(self, my_moves, opp_moves, depth=5):
-        print(my_moves)
-        print(opp_moves)
-        return max(my_moves, key=lambda move: move.base_power)
-    
+        opp_movelist = [opp_moves[k] for k in opp_moves]
+        player = Minimax(my_moves, opp_movelist)
+        return player.play_best_move(depth)
+
     def choose_move(self, battle):
         if battle.available_moves:
-            # best_move = max(battle.available_moves, key=lambda move: move.base_power)
             best_move = self.play_minimax(battle.available_moves, battle.opponent_active_pokemon.moves)
             return self.create_order(best_move)
         else:
             return self.choose_random_move(battle)
 
-class RandomisedMinimaxPlayer(RandomPlayer):
-    def play_minimax(self, my_moves, opp_moves, depth=5):
-        print(my_moves)
-        print(opp_moves)
-        return max(my_moves, key=lambda move: move.base_power)
-    
-    def choose_move(self, battle):
-        if battle.available_moves:
-            # best_move = max(battle.available_moves, key=lambda move: move.base_power)
-            best_move = self.play_minimax(battle.available_moves, battle.opponent_active_pokemon.moves)
-            return self.create_order(best_move)
-        else:
-            return self.choose_random_move(battle)
