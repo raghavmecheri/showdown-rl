31f88de0b8256424a4b96b54011a83dcaaa0ff5e
Raghav Mecheri (Linux) <raghav@mecheri.in>
2021-04-20 18:59:06 +0000

    Added logs

--- experiments.sh
+++ experiments.sh
@@ -1,7 +1,14 @@
 rm -rf logs
 mkdir logs
+
 python main.py -m dqn -c rand_maxrand > logs/rand_maxrand_logs.txt
 python main.py -m dqn -c max_maxrand > logs/max_maxrand_logs.txt
 python main.py -m dqn -c maxrand_maxrand > logs/maxrand_maxrand_logs.txt
 python main.py -m dqn -c minimax_maxrand > logs/minimax_maxrand_logs.txt
 python main.py -m dqn -c minimaxrand_maxrand > logs/minimaxrand_maxrand_logs.txt
+
+python main.py -m dqn -c rand_rand > logs/rand_rand_logs.txt
+python main.py -m dqn -c max_rand > logs/max_rand_logs.txt
+python main.py -m dqn -c maxrand_rand > logs/maxrand_rand_logs.txt
+python main.py -m dqn -c minimax_rand > logs/minimax_rand_logs.txt
+python main.py -m dqn -c minimaxrand_rand > logs/minimaxrand_rand_logs.txt
